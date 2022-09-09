b96bc74c98f58de43998c8481484f72da3fb9713
ahmet dalhançer <ahmed44dalhancer@hotmail.com>
2022-09-09 15:16:09 +0300

    ByDynamicSearc

---
+++ Application/Features/BrandModels/Queries/GetListBrandModelByDynamic/GetListBrandModelByDynamicQuery.cs
@@ -0,0 +1,46 @@
+﻿using Application.Features.BrandModels.Models;
+using Application.Services.Repositories;
+using AutoMapper;
+using Core.Application.Requests;
+using Core.Persistence.Dynamic;
+using Core.Persistence.Paging;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.BrandModels.Queries.GetListBrandModelByDynamic
+{
+    public class GetListBrandModelByDynamicQuery : IRequest<BrandModelListModel>
+    {
+        public PageRequest PageRequest { get; set; }
+        public Dynamic Dynamic { get; set; }
+
+        public class GetListBrandModelByDynamicQueryHandler : IRequestHandler<GetListBrandModelByDynamicQuery, BrandModelListModel>
+        {
+            IBrandModelRepository _brandModelRepository;
+            IMapper _mapper;
+
+            public GetListBrandModelByDynamicQueryHandler(IBrandModelRepository brandModelRepository, IMapper mapper)
+            {
+                _brandModelRepository = brandModelRepository;
+                _mapper = mapper;
+            }
+
+            public async Task<BrandModelListModel> Handle(GetListBrandModelByDynamicQuery request, CancellationToken cancellationToken)
+            {
+               IPaginate<BrandModel> brandModel=await _brandModelRepository.GetListByDynamicAsync(
+                    dynamic: request.Dynamic,
+                    include: m => m.Include(c => c.Brand), 
+                    index: request.PageRequest.Page, 
+                    size: request.PageRequest.PageSize);
+               BrandModelListModel brandModelListModel= _mapper.Map<BrandModelListModel>(brandModel);
+                return brandModelListModel;
+            }
+        }
+    }
+}
--- WebAPI/Controllers/BrandModelsController.cs
+++ WebAPI/Controllers/BrandModelsController.cs
@@ -1,6 +1,8 @@
 ﻿using Application.Features.BrandModels.Models;
 using Application.Features.BrandModels.Queries.GetListBrandModel;
+using Application.Features.BrandModels.Queries.GetListBrandModelByDynamic;
 using Core.Application.Requests;
+using Core.Persistence.Dynamic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
@@ -12,9 +14,17 @@ namespace WebAPI.Controllers
         [HttpGet("getall")]
         public async Task<IActionResult> GetAll([FromQuery] PageRequest pageRequest)
         {
-          GetListBrandModelQuery getListBrandModelQuery= new GetListBrandModelQuery { PageRequest = pageRequest };
+          GetListBrandModelQuery getListBrandModelQuery= new GetListBrandModelQuery { PageRequest=pageRequest};
             BrandModelListModel result = await Mediator.Send(getListBrandModelQuery);
             return Ok(result);
+        }
+        [HttpPost("ByDynamic")]
+        public async Task<IActionResult> GetAllByDynamic([FromQuery] PageRequest pageRequest, [FromBody] Dynamic dynamic)
+        {
+            GetListBrandModelByDynamicQuery getListBrandModelByDynamicQuery =
+                new GetListBrandModelByDynamicQuery { PageRequest=pageRequest,Dynamic=dynamic };
+            BrandModelListModel brandModelListModel= await Mediator.Send(getListBrandModelByDynamicQuery);
+            return Ok(brandModelListModel);
         }
     }
 }
