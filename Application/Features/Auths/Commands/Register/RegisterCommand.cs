c6b19990d207656993514f2c86b5249dd2cdea0a
ahmet dalhançer <ahmed44dalhancer@hotmail.com>
2022-10-14 12:26:57 +0300

    14/10/2022

---
+++ Application/Features/Auths/Commands/Register/RegisterCommand.cs
@@ -0,0 +1,30 @@
+﻿using Application.Features.Auths.Dtos;
+using Application.Features.Auths.Rules;
+using Core.Security.Dtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Auths.Commands.Register
+{
+    public class RegisterCommand : IRequest<RegisterDto>
+    {
+        public string Email { get; set; }
+        public UserForRegisterDto UserForRegisterDto { get; set; }
+        public string IpAdress { get; set; }
+
+
+        public class RegisterCommandHandler : IRequestHandler<RegisterCommand, RegisterDto>
+        {
+            private readonly AuthBusinnessRules _authBusinnessRules;
+
+            public async Task<RegisterDto> Handle(RegisterCommand request, CancellationToken cancellationToken)
+            {
+                await _authBusinnessRules.EmailCanNotBeDublicatedWhenRegistered(request.Email);
+            }
+        }
+    }
+}
---
+++ Application/Services/AuthService/AuthManager.cs
@@ -0,0 +1,49 @@
+﻿using Application.Services.Repositories;
+using Core.Persistence.Paging;
+using Core.Security.Entities;
+using Core.Security.JWT;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.AuthService
+{
+    public class AuthManager : IAuthService
+    {
+        private readonly ITokenHelper _tokenHelper;
+        private readonly IUserOperationClaimRepository _userOperationClaimRepository;
+        private readonly IRefreshTokenRepository _refreshTokenRepository;
+
+        public AuthManager(ITokenHelper tokenHelper, IUserOperationClaimRepository userOperationClaimRepository = null, IRefreshTokenRepository refreshTokenRepository = null)
+        {
+            _tokenHelper = tokenHelper;
+            _userOperationClaimRepository = userOperationClaimRepository;
+            _refreshTokenRepository = refreshTokenRepository;
+        }
+
+        public async Task<RefreshToken> AddRefreshToken(RefreshToken refreshToken)
+        {
+            RefreshToken addedRefreshToken = await _refreshTokenRepository.AddAsync(refreshToken);
+            return addedRefreshToken;
+        }
+
+        public async Task<AccessToken> CreateAccessToken(User user)
+        {
+            IPaginate<UserOperationClaim> userOperationClaims = await _userOperationClaimRepository.GetListAsync(u => u.UserId == user.Id, include: u => u.Include(u => u.OperationClaim));
+
+            IList<OperationClaim> operationClaims = userOperationClaims.Items.Select(u => new OperationClaim { Id = u.OperationClaim.Id, Name = u.OperationClaim.Name }).ToList();
+            AccessToken accessToken = _tokenHelper.CreateToken(user, operationClaims);
+            return accessToken;
+        }
+
+        public Task<RefreshToken> CreateRefreshToken(User user, string ipAdress)
+        {
+            RefreshToken refreshToken = _tokenHelper.CreateRefreshToken(user, ipAdress);
+            return Task.FromResult(refreshToken);
+        }
+    }
+}
