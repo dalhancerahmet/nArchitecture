5fd49c2c69a80075fe759eb762a5e7dbbcff8971
ahmet dalhançer <ahmed44dalhancer@hotmail.com>
2022-10-14 13:57:54 +0300

    14/10/2022 - 2

--- Application/ApplicationServiceRegistration.cs
+++ Application/ApplicationServiceRegistration.cs
@@ -1,4 +1,6 @@
-﻿using Application.Features.Brands.Rules;
+﻿using Application.Features.Auths.Rules;
+using Application.Features.Brands.Rules;
+using Application.Services.AuthService;
 using Core.Application.Pipelines.Validation;
 using FluentValidation;
 using MediatR;
@@ -16,11 +18,11 @@ namespace Application
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
-
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddMediatR(Assembly.GetExecutingAssembly());
 
             services.AddScoped<BrandBusinessRules>();
+            services.AddScoped<AuthBusinnessRules>();
 
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehavior<,>));
@@ -28,6 +30,9 @@ namespace Application
             //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CacheRemovingBehavior<,>));
             //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
+
+            services.AddScoped<IAuthService, AuthManager>();
+
 
             return services;
 
--- Application/Features/Auths/Commands/Register/RegisterCommand.cs
+++ Application/Features/Auths/Commands/Register/RegisterCommand.cs
@@ -1,6 +1,11 @@
 ﻿using Application.Features.Auths.Dtos;
 using Application.Features.Auths.Rules;
+using Application.Services.AuthService;
+using Application.Services.Repositories;
 using Core.Security.Dtos;
+using Core.Security.Entities;
+using Core.Security.Hashing;
+using Core.Security.JWT;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -12,18 +17,45 @@ namespace Application.Features.Auths.Commands.Register
 {
     public class RegisterCommand : IRequest<RegisterDto>
     {
-        public string Email { get; set; }
         public UserForRegisterDto UserForRegisterDto { get; set; }
-        public string IpAdress { get; set; }
+        public string IpAddress { get; set; }
 
 
         public class RegisterCommandHandler : IRequestHandler<RegisterCommand, RegisterDto>
         {
             private readonly AuthBusinnessRules _authBusinnessRules;
+            private readonly IAuthService _authService;
+            private readonly IUserRepository _userRepository;
+
+            public RegisterCommandHandler(IAuthService authService, IUserRepository userRepository)
+            {
+                _authService = authService;
+                _userRepository = userRepository;
+            }
 
             public async Task<RegisterDto> Handle(RegisterCommand request, CancellationToken cancellationToken)
             {
-                await _authBusinnessRules.EmailCanNotBeDublicatedWhenRegistered(request.Email);
+               // await _authBusinnessRules.EmailCanNotBeDublicatedWhenRegistered(request.UserForRegisterDto.Email);
+                byte[] passwordHash, passwordSalt;
+                HashingHelper.CreatePasswordHash(request.UserForRegisterDto.Password, out passwordHash, out passwordSalt);
+                User newUser = new()
+                {
+                    FirstName = request.UserForRegisterDto.FirstName,
+                    LastName = request.UserForRegisterDto.LastName,
+                    Email = request.UserForRegisterDto.Email,
+                    PasswordHash = passwordHash,
+                    PasswordSalt = passwordSalt,
+                };
+                await _userRepository.AddAsync(newUser);
+                AccessToken accessToken = await _authService.CreateAccessToken(newUser);
+                RefreshToken refreshToken = await _authService.CreateRefreshToken(newUser, request.IpAddress);
+                RefreshToken addedRefreshToken = await _authService.AddRefreshToken(refreshToken);
+
+                return new()
+                {
+                    AccessToken = accessToken,
+                    RefreshToken = refreshToken,
+                };
             }
         }
     }
